b6d6ff2a23465a2bc50877acac46f57f2e03c1cb
Sakib Noman <skbnmn@gmail.com>
2021-03-07 19:39:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route, Switch
+} from "react-router-dom";
+import Footer from '../src/Components/Footer/Footer';
+import Header from '../src/Components/Header/Header';
 import './App.css';
+import Countries from "./Components/Countries/Countries";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Header></Header>
+        <Switch>
+          <Route exact path="/" >
+            <Countries></Countries>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/CountryDetails/CountryDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CountryDetails = () => {
+    return (
+        <div>
+            <h3>Country Details</h3>
+        </div>
+    );
+};
+
+export default CountryDetails;
